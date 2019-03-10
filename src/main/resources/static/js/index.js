0ad5569f4be9ecf8e06722c5ef5ceb67bc299dad
Skylight <907310977@qq.com>
2019-03-11 00:51:10 +0800

    <更新>添加获取某一文章内容的JS函数
    现在点击文章简介下的按钮可以获取文章内容了

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,5 +1,4 @@
 $(function(){
-
     alert("你好呀！");
     //haha();
     //LoadIndex();
@@ -10,41 +9,69 @@ function haha()
     alert("1111111111111");
 }
 
-function LoadIndex()
-{
+function LoadIndex() {
     $.ajax({
-        url:"/summarys",
-        type:"POST",
-        async:false,
-        data:{},
+        url: "/summarys",
+        type: "POST",
+        async: false,
+        data: {},
 
-        success:function(data){
+        success: function (data) {
 
             console.log("成功了！");
 
             var content = "";
 
-            for(var i in data)
-            {
+            for (var i in data) {
                 var id = data[i].id;
                 var topic = data[i].topic;
                 var summary = data[i].summary;
                 var articleId = data[i].articleId;
 
-               content = content+
-                   '<div class="col-xs-6 col-lg-4" id="lastOne">'+
-                   '<h2>'+topic+'</h2>'+
-                   '<p>'+summary+'</p>'+
-                   '<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>'+
-                   '</div>';
+                content = content +
+                    '<div class="col-xs-6 col-lg-4" id="lastOne">' +
+                    '<h2>' + topic + '</h2>' +
+                    '<p>' + summary + '</p>' +
+                    '<p><a class="btn btn-default" href="' + 'javascript:GetArticleById(' + articleId + ');' + '" role="button">View details &raquo;</a></p>' +
+                    '</div>';
             }
 
 
             $("#lastOne").html(content);
 
-        },error:function(){
+        }, error: function () {
             alert("数据加载错误");
         }
 
     });
 }
+
+    function GetArticleById(id)
+    {
+        $.ajax({
+            url:"/article",
+            type:"POST",
+            async:false,
+            data:{id:id},
+
+            success:function(data){
+                    console.log("成功了！");
+                    var content = "";
+                    var id = data.id;
+                    var hi = data.content;
+
+                    content = content + '<div class="col-xs-6 col-lg-4">'+
+                        '<h2>'+id+'</h2>'+
+                        '<p>'+hi+'</p>'+
+                    '</div>';
+
+                    $("#lastOne").html(content);
+            },error:function(){
+                alert("数据加载错误");
+            }
+
+        });
+
+
+
+    }
