0de9a660035e3db6ff798cd15e593ddae691dcae
Skylight <907310977@qq.com>
2019-03-13 17:58:16 +0800

    <更新>添加拦截器

---
+++ src/main/java/com/skylight/blog/config/MyWebMvcConfigurerAdapter.java
@@ -0,0 +1,27 @@
+package com.skylight.blog.config;
+
+import com.skylight.blog.interceptor.WebInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+@Configuration
+public class MyWebMvcConfigurerAdapter extends WebMvcConfigurerAdapter {
+
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/art").setViewName("article.html");
+        super.addViewControllers(registry);
+    }
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new WebInterceptor()).addPathPatterns("/summarys");
+        super.addInterceptors(registry);
+        System.out.println("===========   拦截器注册完毕   ===========");
+    }
+
+
+}
---
+++ src/main/java/com/skylight/blog/interceptor/WebInterceptor.java
@@ -0,0 +1,27 @@
+package com.skylight.blog.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class WebInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        System.out.println("==============  request before  ==============");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+        System.out.println("==============  request  ==============");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+        System.out.println("==============  request completion  ==============");
+    }
+
+}
