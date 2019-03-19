f47e63759101b0fd5e57e81b04bafe71e9115ade
Skylight <907310977@qq.com>
2019-03-19 22:41:47 +0800

    <修改>修改加载首页的JS函数

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,39 +1,46 @@
 $(function(){
     alert("你好呀！");
-    //haha();
-    //LoadIndex();
+    LoadPage(1);
 });
 
-function haha()
-{
-    alert("1111111111111");
-}
-
-function LoadIndex() {
+function LoadPage(page) {
     $.ajax({
-        url: "/summarys",
+        url: "/articleInfoDetailsByCategoryId",
         type: "POST",
         async: false,
-        data: {},
+        data: {page:page},
 
         success: function (data) {
-
             console.log("成功了！");
 
             var content = "";
+            var labels = "";
 
             for (var i in data) {
-                var id = data[i].id;
-                var topic = data[i].topic;
+                var id = data[i].articleInfoId;
+                var createTime = data[i].createBy
+                var category = data[i].category.name;
+                var title = data[i].title;
                 var summary = data[i].summary;
-                var articleId = data[i].articleId;
+
+                for(var j=0;j<data[i].articleLabelList.length;j++)
+                {
+                    labels = labels + '<span class="glyphicon glyphicon-paperclip"></span> ' + data[i].articleLabelList[j].label.name + '&#8194;';
+                }
 
                 content = content +
-                    '<div class="col-xs-12 col-lg-12" id="lastOne">' +
-                    '<h2>' + topic + '</h2>' +
-                    '<p>' + summary + '</p>' +
-                    '<p><a class="btn btn-default" href="' + 'javascript:GetArticleById(' + articleId + ');' + '" role="button">View details &raquo;</a></p>' +
-                    '</div>';
+                    '<div class="col-xs-12 col-lg-12 shadow distance">'+
+                    '                    <h3>'+title+'</h3>'+
+                    '                    <p>'+
+                    '                    <span class="glyphicon glyphicon-time"></span>'+createTime+ '&#8197;'+
+                    '                    <span class="glyphicon glyphicon-user"></span> 原创'+ '&#8197;'+
+                    '                    <span class="glyphicon glyphicon-tags"></span> '+category+'</p>'+
+                    '                    <p>'+ summary +'</p>'+
+                    '                    <p><a class="btn btn-primary" href="/article.html?id='+ id + '" role="button">View details &raquo;</a></p>'+
+                    '                    <hr>'+
+                    '                    <p>'+ labels +'</p>'+
+                    '                </div>';
+                labels = "";
             }
 
 
@@ -46,32 +53,3 @@ function LoadIndex() {
     });
 }
 
-    function GetArticleById(id)
-    {
-        $.ajax({
-            url:"/article",
-            type:"POST",
-            async:false,
-            data:{id:id},
-
-            success:function(data){
-                    console.log("成功了！");
-                    var content = "";
-                    var id = data.id;
-                    var hi = data.content;
-
-                    content = content + '<div class="col-xs-12 col-lg-12">'+
-                        '<h2>'+id+'</h2>'+
-                        '<p>'+hi+'</p>'+
-                    '</div>';
-
-                    $("#lastOne").html(content);
-            },error:function(){
-                alert("数据加载错误");
-            }
-
-        });
-
-
-
-    }
