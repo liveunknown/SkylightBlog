fd13652f44dde4652cad4ea63f017ba986141584
Skylight <907310977@qq.com>
2019-03-23 17:48:39 +0800

    <更新>完善ArticleInfo和ArticleContent的Mapper方法

--- src/main/java/com/skylight/blog/mapper/ArticleInfoMapper.java
+++ src/main/java/com/skylight/blog/mapper/ArticleInfoMapper.java
@@ -1,5 +1,6 @@
 package com.skylight.blog.mapper;
 
+import com.skylight.blog.model.ArticleContent;
 import com.skylight.blog.model.ArticleInfo;
 import com.skylight.blog.model.ArticleWrap;
 import org.apache.ibatis.annotations.Mapper;
@@ -15,7 +16,19 @@ public interface ArticleInfoMapper {
 
     ArticleWrap getArticleInfoDetailByArticleInfoId(@Param("id")Long id);
 
+    // Only ArticleInfo
     boolean addArticleInfo(@Param("ai")ArticleInfo articleInfo);
 
+    boolean deleteArticleInfo(@Param("id")Long id);
+
     ArticleInfo getArticleInfoById(@Param("id")Long id);
+
+    boolean updateArticleInfo(@Param("ai")ArticleInfo articleInfo);
+
+    // Only ArticleContent    No need for query
+    boolean addArticleContent(@Param("ac")ArticleContent articleContent);
+
+    boolean deleteArticleContent(@Param("id")Long id);
+
+    boolean updateArticleContent(@Param("ac")ArticleContent articleContent);
 }
