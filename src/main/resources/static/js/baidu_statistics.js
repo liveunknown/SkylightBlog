3ae86a7aaa484e58d560da283c6274d0392ef736
Skylight <907310977@qq.com>
2019-06-03 18:55:38 +0800

    <更新>在前端页面加入百度统计JS代码

---
+++ src/main/resources/static/js/baidu_statistics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?b74c9a65f6cbd16c5f57ea993b1a1623";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
