d92e9ae77d10715bc1c96d05801ac13e4bf5d6fd
Skylight <907310977@qq.com>
2019-06-06 11:25:49 +0800

    <添加>添加更新、友链、关于页面

---
+++ src/main/resources/static/js/footer.js
@@ -0,0 +1,6 @@
+document.writeln("<footer>");
+document.writeln("        <br>");
+document.writeln("        <p class=\'text-center\'>&copy;2019-2020  <b>Baidurex.com</b> <span class=\'glyphicon glyphicon-tower\'></span> San Junipero 版权所有</p>");
+document.writeln("        <p class=\'text-center\'><b><a href=\'http://www.beian.miit.gov.cn\' style=\'color: black\'>豫ICP备19005657号</a></b></p>");
+document.writeln("    </footer>");
+document.writeln("");
