77d7cb03c2e0e23e0703eeef7ca5519528a72496
Skylight <907310977@qq.com>
2019-06-17 05:50:28 +0800

    <移动>将后台管理页移到admin目录下

--- src/main/java/com/skylight/blog/config/MyWebMvcConfigurerAdapter.java
+++ src/main/java/com/skylight/blog/config/MyWebMvcConfigurerAdapter.java
@@ -18,7 +18,9 @@ public class MyWebMvcConfigurerAdapter extends WebMvcConfigurerAdapter {
     }
 
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new WebInterceptor()).addPathPatterns("/admin/**").excludePathPatterns("/admin/login");
+/*      registry.addInterceptor(new WebInterceptor()).addPathPatterns("/admin/**").excludePathPatterns("/admin/login");
+        registry.addInterceptor(new VisitorInterceptor()).addPathPatterns("/**").excludePathPatterns("/login.html", "/admin/**","/index","/js/**");*/
+        registry.addInterceptor(new WebInterceptor()).addPathPatterns("/admins/**").excludePathPatterns("/admins/login");
         registry.addInterceptor(new VisitorInterceptor()).addPathPatterns("/**").excludePathPatterns("/login.html", "/admin/**","/index","/js/**");
         super.addInterceptors(registry);
         System.out.println("===========   拦截器注册完毕   ===========");
